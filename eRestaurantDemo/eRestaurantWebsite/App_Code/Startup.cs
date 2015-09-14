5630566517dbdedb4157f5e010f77e38c8d04063
zzisha1 <zisha_cse2008@yahoo.com>
2015-09-14 16:26:39 -0600

    setup of new repo structure and demo VS project

---
+++ eRestaurantDemo/eRestaurantWebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eRestaurantWebsite.Startup))]
+namespace eRestaurantWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
